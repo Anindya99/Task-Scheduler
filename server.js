fa71db4f3b137770316c6366a28372ace6a6281b
Anindya Sadhukhan <anindyasadhukhan1999@gmail.com>
2022-02-27 11:02:57 +0530

    cofigured heroku deployment

--- server.js
+++ server.js
@@ -56,5 +56,14 @@ app.use((_, res, next) => {
     next(err);
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`server started at post ${port}`));
