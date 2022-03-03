aff55bf5898d9be05a3846f3bab255a99514d88e
Anindya Sadhukhan <anindyasadhukhan1999@gmail.com>
2022-03-03 21:39:36 +0530

    modified ui

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const Cover = () => {
   return (
     <div className='cover'>
-    <div className='box1'>
+    <div className='box1 flex-item'>
       <img src="icons/time2.png" alt="cover" className="coverphoto"></img>
     </div>
-   <div className='box2'>
+   <div className='box2 flex-item'>
      <blockquote>
     Your greatest resource is your time .
     <span>Brian Tracy</span>
     </blockquote>
    </div>
-   <div className='box3'>
+   <div className='box3 flex-item'>
    <Link to="/login" style={{color: 'inherit', textDecoration: 'none' }}><p className='box3text'><span>Log in to Task Scheduler</span></p></Link>
    </div>
   </div>
